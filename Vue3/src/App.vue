937e84cfebc8022a63a00716689b30affe3b9dde
Chen Nian <1990493833@qq.com>
2025-02-19 12:59:53 +0800

    02_Vue3组合式

--- Vue3/src/App.vue
+++ Vue3/src/App.vue
@@ -1,14 +1,14 @@
 <template>
     <!-- 这里写html内容 -->
-     <div class="app">
-        <h1>你好啊！</h1>
-     </div>
+        <person></person>
 </template>
 
 <script>
 // 这里写JS或者TS代码,Vue推荐使用Ts书写
+    import Person from './components/Preson.vue' 
     export default{
-        name:"App"  // 这里是组件名
+        name:"App",  // 这里是组件名
+        components:{Person}// 注册组件
     }
 </script>
 
@@ -19,5 +19,8 @@
         box-shadow: 0 0 10px;
         border-radius: 10px;
         padding: 20px;
+    }
+    .button{
+        margin: 0 5px;
     }
 </style>
---
+++ Vue3/src/components/Preson.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="person">
+    <h1>姓名:{{ name }}</h1>
+    <h2>年龄:{{ age }}</h2>
+    <button @click = "showTel">查看联系方式</button>
+    <button @click = "changeName">修改名字</button>
+    <button @click= "changeAge">修改年龄</button>
+    </div>
+    <!-- 这里是模版的写法，也就是我的前端页面当中要给出那些东西 -->
+</template>
+
+<script lang="ts">
+    export default{
+        name:'Person',
+        setup(){
+            // 数据
+            let name = "张三"
+            let age = 18
+            let tel = '13912215493'
+            // 这种写法不是响应式的，在后期进行更新的时候不会进行更新
+
+            // 方法
+            function changeName(){
+                name = "李四"
+            }
+            function changeAge(){
+                name += 1
+            }
+            function showTel(){
+                alert("tel")
+            }
+            return{name:name,age:age,changeName,changeAge,showTel}
+
+        }
+        // 脚本当中存着相应的数据，这种写法就是组合式写法，需要通过对象来进行调用
+    }
+</script>
+
+<style scoped>
+    .person{
+        background-color: yellow;
+        box-shadow: 0 0 10px;
+        border-radius:10px;
+        padding: 20px;
+    }
+</style>
